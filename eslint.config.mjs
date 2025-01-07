a285373a5eb36d9e0a62a8f33b8f162faaec708a
hyy <coderhyy@gmail.com>
2025-01-07 10:29:34 +0800

    chore: update ESLint configuration to allow console warnings and add source language setting for i18n

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,11 @@ const eslintConfig = [
     "next/typescript",
     "plugin:perfectionist/recommended-natural-legacy"
   ),
+  {
+    rules: {
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
 ];
 
 export default eslintConfig;
